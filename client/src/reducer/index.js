2315cb339081e7e7195a9360a43cb1b8ea17c719
“talhalalala” <talhasadak@outlook.comgit config --global user.name Talhalalala>
2022-01-18 18:25:42 +0000

    reducer written

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -12,7 +12,27 @@ const gameReducer = (state = initState, action) => {
                     ...state, 
                     players: action.payload.map(player => ({player: player, ready:false}))
             })
+
+        case 'PLAYER_READY':
+            const toggle = state.players.find(p => p.player.id === action.payload)
+            const readyPlayer = [
+                ...state.players.slice(0, state.players.indexOf(toggle)),
+                { ...toggle, userReady: !toggle.userReady},
+                ...state.players.slice(state.players.indexOf(toggle) + 1)
+            ]
+            return ({
+                 ...state, 
+                 player: player
+            })
         
+        case 'PLAYER_NOT_READY':
+            const notReady = [...state.players.map(player => ({ player: player.player, userReady: false}))]
+            return ({
+                ...state, 
+                player: notReady
+            })
+
+
         case 'LOAD_QUESTIONS':
             return ({
                 ...state,
