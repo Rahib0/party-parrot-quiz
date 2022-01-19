d0017979e9a7c9e6e25e5b3a5eaaa682fbdb5259
“talhalalala” <talhasadak@outlook.comgit config --global user.name Talhalalala>
2022-01-19 20:21:49 +0000

    homepage and create game pages now size responsive

--- client/src/Pages/CreateGame/index.js
+++ client/src/Pages/CreateGame/index.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import { Back, FormCreateGame } from '../../components'
 import '../../Styles/createGame/creategame.css';
+import { parrot } from '../../Styles/manyparrots';
+
 
 export default function CreateGame() {
     return (
         <div className='create-game'>
-            <h1>Create your Game</h1>
+            <table>
+                <tbody>
+                    <td><img src={parrot} alt='party-parrot' /></td>                    
+                    <td>            
+                        <h1>Create your Game</h1>
+                    </td>
+                    <td><img src={parrot} alt='party-parrot' /></td>
+                </tbody>
+            </table>
             <FormCreateGame />
             <div className='bck-btn'>
                 <Back />
