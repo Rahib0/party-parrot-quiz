eb8c41c0309359a1fd4483daada19c7e3f7106e7
Souheil  Fenghour <souheil@getfutureproof.co.uk>
2022-01-20 18:26:01 +0000

    fix server deployment

--- server/db/server.js
+++ server/db/server.js
@@ -1,4 +1,5 @@
 const db = connect("mongodb://localhost:27017/games");
 
 db.games.drop();
+//db.question.insertOne({id:1,question:{category:"Entertainment: Books",difficulty:"medium",question: "In the &quot;The Hobb bit&quot;, who kills Smaug?",correct_answer:"Bard",incorrect_answers:["Bilbo Baggins","Gandalf the Grey","Frodo"]}});
 db.question.insertOne({id:1,question:{category:"Entertainment: Books",difficulty:"medium",question: "In the &quot;The Hobb bit&quot;, who kills Smaug?",correct_answer:"Bard",incorrect_answers:["Bilbo Baggins","Gandalf the Grey","Frodo"]}});
